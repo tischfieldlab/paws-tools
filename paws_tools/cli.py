c1a991584287f4091ee2e685006891c03d6fddb8
quantumdot <quantumdot@users.noreply.github.com>
2022-12-07 12:10:20 -0500

    - detangle y-axis inversion from calibration - expose frame height to CLI for y-axis inversion

--- paws_tools/cli.py
+++ paws_tools/cli.py
@@ -6,7 +6,7 @@ import click
 import sleap_io
 import pandas as pd
 
-from paws_tools.slp_to_csv import convert_physical_units, node_positions_to_dataframe
+from paws_tools.slp_to_csv import convert_physical_units, invert_y_axis, node_positions_to_dataframe
 
 
 @click.group()
@@ -16,12 +16,13 @@ def cli():
     pass  # pylint: disable=unnecessary-pass
 
 
-@cli.command(name="slp-to-paws-csv", short_help="convert SLEAP .slp file to PAWS importable csv files")
+@cli.command(name="slp-to-paws-csv", short_help="Convert SLEAP .slp file to PAWS importable csv files")
 @click.argument("slp_file", type=click.Path(exists=True, dir_okay=False))
 @click.option("-bp", "--body-part", default="Toe", help="Name of the body part to extract")
 @click.option("--cal-node1", default="Top_Box", help="Name of calibration point one")
 @click.option("--cal-node2", default="Bot_Box", help="Name of calibration point two")
-@click.option("--cal-dist", default=1.0, type=float, help="physical distance between --cal-node1 and --cal-node2")
+@click.option("--cal-dist", default=1.0, type=float, help="Physical distance between --cal-node1 and --cal-node2")
+@click.option("--frame-height", default=512, type=int, help="Pixel height of video frames")
 @click.option(
     "--dest-dir", default=os.getcwd(), type=click.Path(file_okay=False), help="Name of the body part to extract"
 )
@@ -33,6 +34,7 @@ def slp_to_paws_csv(slp_file, body_part, cal_node1, cal_node2, cal_dist, dest_di
     labels = sleap_io.load_slp(slp_file)
 
     # convert labels coords to physical units
+    labels = invert_y_axis(labels, 512)
     labels = convert_physical_units(labels, cal_node1, cal_node2, cal_dist)
     coords = node_positions_to_dataframe(labels, body_part)
 
--- paws_tools/slp_to_csv.py
+++ paws_tools/slp_to_csv.py
@@ -18,7 +18,6 @@ def node_positions_to_dataframe(labels: Labels, node_name: str = "Toe") -> pd.Da
     data = []
     node = labels.skeletons[0][node_name]
     for frame in labels.labeled_frames:
-
         data.append(
             {
                 "video": frame.video.filename,
@@ -31,9 +30,28 @@ def node_positions_to_dataframe(labels: Labels, node_name: str = "Toe") -> pd.Da
     return pd.DataFrame(data)
 
 
-def convert_physical_units(
-    labels: Labels, top_node: str, bot_node: str, pix_height: int, true_distance: float
-) -> Labels:
+def invert_y_axis(labels: Labels, frame_height: int) -> Labels:
+    """Invert the Y-coordinates such that the origin is switched between bottom-left and top-left
+
+    If the origin is bottom-left, the resulting origin will be top-left.
+    If the origin is top-left, the resulting origin will be bottom-left.
+
+    Args:
+        labels: labels instance to convert
+        frame_height: height of the video frames
+
+    Returns:
+        Labels instance with point units converted to physical distances
+    """
+    for frame in labels.labeled_frames:
+        for instance in frame.predicted_instances:
+            for key, val in instance.points.items():
+                instance.points[key].y = frame_height - val.y
+
+    return labels
+
+
+def convert_physical_units(labels: Labels, top_node: str, bot_node: str, true_dist: float) -> Labels:
     """Converts the coordinates in `labels` from px to physical distance units (i.e. millimeters).
 
     Args:
@@ -53,15 +71,14 @@ def convert_physical_units(
         box_cords = labels.numpy(video)[:, 0, (Top_index, Bot_index), 1]
 
         box_median = np.nanmedian(box_cords, axis=0)
-        mm2px = true_distance / abs(np.diff(box_median))
+        mm2px = true_dist / abs(np.diff(box_median))
         conv_factors[video.filename] = mm2px[0]
 
     for frame in labels.labeled_frames:
         mm2px = conv_factors[frame.video.filename]
-        height = 512
         for instance in frame.predicted_instances:
             for key, val in instance.points.items():
                 instance.points[key].x = val.x * mm2px
-                instance.points[key].y = (height - val.y) * mm2px
+                instance.points[key].y = val.y * mm2px
 
     return labels
